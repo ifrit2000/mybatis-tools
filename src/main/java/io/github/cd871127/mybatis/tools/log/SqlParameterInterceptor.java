81ebfcc7c0c7a9f830fc1606b234162fd70fcde5
Anthony <cdistc@163.com>
2020-06-28 13:58:06 +0800

    处理日志中sql参数的拦截器

---
+++ src/main/java/io/github/cd871127/mybatis/tools/log/SqlParameterInterceptor.java
@@ -0,0 +1,130 @@
+package io.github.cd871127.mybatis.tools.log;
+
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.regex.Matcher;
+
+/**
+ * 在日志中打印完整的Sql（替换sql中的？）
+ */
+@Intercepts(value = {
+        @Signature(type = Executor.class,
+                method = "update",
+                args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class,
+                method = "query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class,
+                        CacheKey.class, BoundSql.class})})
+public class SqlParameterInterceptor implements Interceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(SqlParameterInterceptor.class);
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        try {
+            MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+            Object parameter = null;
+            if (invocation.getArgs().length >= 1) {
+                parameter = invocation.getArgs()[1];
+            }
+            // BoundSql就是封装myBatis最终产生的sql类
+            BoundSql boundSql = mappedStatement.getBoundSql(parameter);
+            // 获取节点的配置
+            Configuration configuration = mappedStatement.getConfiguration();
+            // 获取到最终的sql语句
+            String sql = parseSql(configuration, boundSql);
+            log.debug(mappedStatement.getId());
+            log.debug(sql);
+        } catch (Exception e) {
+            log.warn("show sql parameter failed.");
+        }
+        //执行sql
+        return invocation.proceed();
+    }
+
+
+    /**
+     * 如果参数是String，则添加单引号， 如果是日期，则转换为时间格式器并加单引号； 对参数是null和不是null的情况作了处理
+     *
+     * @param parameter
+     * @return
+     */
+    private String handleParameter(Object parameter) {
+        String value;
+        if (parameter == null) {
+            value = "null";
+        } else if (parameter instanceof String) {
+            value = "'" + parameter.toString() + "'";
+        } else if (parameter instanceof Date) {
+            DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
+            value = "'" + formatter.format(new Date()) + "'";
+        } else {
+            value = parameter.toString();
+        }
+        return value;
+    }
+
+
+    /**
+     * 处理sql
+     *
+     * @param configuration
+     * @param boundSql
+     * @return
+     */
+    private String parseSql(Configuration configuration, BoundSql boundSql) {
+        Object parameterObject = boundSql.getParameterObject();  // 获取参数
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        // sql语句中多个空格都用一个空格代替
+        String sql = boundSql.getSql().replaceAll("[\\s]+", " ");
+        if (parameterMappings != null && parameterMappings.size() > 0 && parameterObject != null) {
+            // 获取类型处理器注册器，类型处理器的功能是进行java类型和数据库类型的转换
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            // 如果根据parameterObject.getClass(）可以找到对应的类型，则替换
+            if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(handleParameter(parameterObject)));
+            } else {
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);// MetaObject主要是封装了originalObject对象，提供了get和set的方法用于获取和设置originalObject的属性值,主要支持对JavaBean、Collection、Map三种类型对象的操作
+                for (ParameterMapping parameterMapping : parameterMappings) {
+                    String propertyName = parameterMapping.getProperty();
+                    if (metaObject.hasGetter(propertyName)) {
+                        Object obj = metaObject.getValue(propertyName);
+                        sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(handleParameter(obj)));
+                    } else if (boundSql.hasAdditionalParameter(propertyName)) {
+                        Object obj = boundSql.getAdditionalParameter(propertyName);  // 该分支是动态sql
+                        sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(handleParameter(obj)));
+                    } else {
+                        //打印出缺失，提醒该参数缺失并防止错位
+                        sql = sql.replaceFirst("\\?", "[Missing]");
+                    }
+                }
+            }
+        }
+        return sql;
+    }
+
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    public void setProperties(Properties properties) {
+
+    }
+}
